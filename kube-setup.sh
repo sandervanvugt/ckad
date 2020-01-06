f6e1642c408502d4fe660a04ab9bab3de7eda0eb
Alexander van Vugt <alexandervanvugt@AlexandersMBP2.localdomain>
2020-01-06 11:57:05 +0100

    jan 2020 changes

--- kube-setup.sh
+++ kube-setup.sh
@@ -24,7 +24,17 @@ echo installing virtualbox
 dnf install make perl kernel-devel gcc elfutils-libelf-devel -y
 dnf install VirtualBox-6.1 -y
 echo installing kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/%60curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt%60/bin/linux/amd64/kubectl 
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+yum install -y kubectl
 
 echo downloading minikube, check version
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
