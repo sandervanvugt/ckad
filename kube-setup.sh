9b15e8a5639a926bdf6531702fa1e325d006bdd4
Sander van Vugt <mail@sandervanvugt.nl>
2020-09-09 15:52:57 +0200

    bugfix

--- kube-setup.sh
+++ kube-setup.sh
@@ -43,7 +43,7 @@ then
 	sudo apt-get upgrade -y
 	sudo apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
 
-	sudo adduser `id -un` libvirtd
+	sudo adduser `id -un` libvirt
 	sudo adduser `id -un` kvm
 fi
 
