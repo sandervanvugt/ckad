05b152a085d47b738cda4e9357ee21cfb3df21ae
piouson <piouson@users.noreply.github.com>
2022-02-23 23:29:56 +0000

    Add minikube-docker-wsl2-setup.sh
    Add minikube setup file for WSL2 Ubuntu

---
+++ minikube-docker-wsl2-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# added 23 Feb. 2022
+# currently only supported on WSL2 - Ubuntu 20.04 LTS
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+####
+echo the script is now ready
+echo manually run "minikube start --vm-driver=docker" to start minikube
+
+sudo usermod -aG docker $USER
+newgrp docker
+
+minikube start --vm-driver=docker --cni=calico
