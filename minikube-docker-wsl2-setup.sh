db799f8a4913d0320e96a707dc483e762713a83d
piouson <piouson@users.noreply.github.com>
2022-03-01 09:23:05 +0000

    Update minikube-docker-wsl2-setup.sh

--- minikube-docker-wsl2-setup.sh
+++ minikube-docker-wsl2-setup.sh
@@ -3,7 +3,6 @@
 # currently only supported on WSL2 - Ubuntu
 
 sudo apt-get update
-sudo apt-get upgrade -y
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
