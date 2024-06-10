d990a65c557a8152e94503f43e79d8cf56304317
sandervanvugt <mail@sandervanvugt.nl>
2024-06-10 22:20:13 +0200

    message

--- setup-kubetools.sh
+++ setup-kubetools.sh
@@ -2,31 +2,57 @@
 # kubeadm installation instructions as on
 # https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
 
-cat <<EOF > /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+# this script supports Ubuntu 20.04 LTS and later only
+# run this script with sudo
+
+#if ! [ $USER = root ]
+#then
+#	echo run this script with sudo
+#	exit 3
+#fi
+
+if ! [ -f /tmp/container.txt ]
+then
+	echo run ./setup-container.sh before running this script
+	exit 4
+fi
+
+# setting MYOS variable
+MYOS=$(hostnamectl | awk '/Operating/ { print $3 }')
+OSVERSION=$(hostnamectl | awk '/Operating/ { print $4 }')
+
+# detecting latest Kubernetes version
+KUBEVERSION=$(curl -s https://api.github.com/repos/kubernetes/kubernetes/releases/latest | jq -r '.tag_name')
+KUBEVERSION=${KUBEVERSION%.*}
+
+
+if [ $MYOS = "Ubuntu" ]
+then
+	echo RUNNING UBUNTU CONFIG
+	cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+	br_netfilter
 EOF
+	
+	sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+	curl -fsSL https://pkgs.k8s.io/core:/stable:/${KUBEVERSION}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+	echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${KUBEVERSION}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sleep 2
 
-# Set SELinux in permissive mode (effectively disabling it)
-setenforce 0
-sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-
-# disable swap (assuming that the name is /dev/centos/swap
-sed -i 's/^\/dev\/mapper\/centos-swap/#\/dev\/mapper\/centos-swap/' /etc/fstab
-swapoff /dev/mapper/centos-swap
-
-yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
-
-systemctl enable --now kubelet
+	sudo apt-get update
+	sudo apt-get install -y kubelet kubeadm kubectl
+	sudo apt-mark hold kubelet kubeadm kubectl
+	sudo swapoff -a
+	
+	sudo sed -i 's/\/swap/#\/swap/' /etc/fstab
+fi
 
 # Set iptables bridging
-cat <<EOF >  /etc/sysctl.d/k8s.conf
+sudo cat <<EOF >  /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
-sysctl --system
+sudo sysctl --system
+
+sudo crictl config --set \
+    runtime-endpoint=unix:///run/containerd/containerd.sock
+echo 'after initializing the control node, follow instructions and use kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml to install the calico plugin (control node only). On the worker nodes, use sudo kubeadm join ... to join'
